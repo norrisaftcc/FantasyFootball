d72383df38787102f4cf65cb2c33afdb4df3f01b
Andrew Norris <norrisa@faytechcc.edu>
2021-11-29 11:01:25 -0500

    Updated plays, CPU now gets a choice of moves

--- footballsim.py
+++ footballsim.py
@@ -8,7 +8,32 @@
 
 # We'll start with a one sided game where the player is always
 # on offense. Their goal is to score a goal or a down.
+# Notes for Football Sim
 
+"""
+Offensive moves:
+run, pass, kick (fg/punt)
+
+Defensive moves:
+rush, pass defense, kick defense
+
+should have a R/P/S type of interaction
+
+Each round:
+- Offense picks move
+- Defense picks move
+- Calculate the results
+
+Possible results:
+- gain or loss (yards)
+- Turnover (ex: interception, fumble)
+
+ex:
+("gain", 7) # for a regular play
+("interception", 0) # intercepted pass
+("turnover", 40)   # other team successfully punts
+
+"""
 import random
 
 def report(currentYardage, currentDown):
@@ -18,6 +43,12 @@ def report(currentYardage, currentDown):
 def getPlayerMove():
     action = input("Choose run, pass, or kick: ")
     return action
+
+def getCPUMove():
+  options = ["Rush", "Pass Defense", "Kick Defense"]
+  # for now, random choice 
+  pick = random.choice(options)
+  return pick
 
 def PlayOneRound():
     """
@@ -34,20 +65,26 @@ def PlayOneRound():
         
         report(currentLine, down)
         move = getPlayerMove()
+        cpuMove = getCPUMove()
+
+        # plays return two values, result and yardage
 
         if move == "run":
             # handle running plays
-            distance = runningPlay()
+            result, distance = runningPlay(cpuMove)
             
         if move == "pass":
             # handle passing plays
-            distance = passingPlay()
+            result, distance = passingPlay(cpuMove)
         if move == "kick":
             # handle field goals, etc
-            distance = fieldGoalAttempt()
+            result, distance = fieldGoalAttempt(cpuMove, currentLine)
     
         # Update the game state
         currentLine = currentLine - distance # counting 50 -> 0
+        print("-" * 40)
+        print("You chose", move, " - CPU chose", cpuMove)
+        print("Play result" , result)
         if distance > 0:
             print("That play gained you", distance, "yards")
         else:
@@ -72,31 +109,76 @@ def PlayOneRound():
     
 
 # Various functions for determining how well plays go
-def runningPlay():
-    """ returns yards gained (or lost, if negative). """
-
-    # This could be anything -
-    # 10% of a bad play and losing yards, 10% awesome play
-    # otherwise middling result
-
-    roll = random.randint(1, 100)
-    if roll < 10: # bad play!
-        distance = 0 - random.randint(1, 10)
-    elif roll > 90: # great play!
-        distance = random.randint(1, 20)
-    else: # middling result
-        distance = random.randint(1, 20) - 10
-    return distance
-
-def passingPlay():
+def runningPlay(cpuMove):
+    """ returns: result type (string), yards gained (or lost, if negative). """
     # idea borrowed from https://github.com/wcastil/football-simulation
     # these are running gains, so they should be moved
+    # base results
     possibleGains = [0,0,1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,9,10,10,11,11,12,12,13,14,15]
-    gainFromPlay = random.choice(possibleGains)
-    return gainFromPlay
+    distance = random.choice(possibleGains)
+    result = "gain"
 
-def fieldGoalAttempt():
-    return random.randint(1, 20) # TODO
+    if cpuMove == "Rush":
+      # for now, run defense just makes running less effective
+      distance = distance - random.randint(1, 5)
+      if distance < 0:
+        result = "loss"
+
+    return result, distance
+
+
+def passingPlay(cpuMove):
+  # the * signs add that many of the entry (for example, 7 zeros)
+  gains = [0]*7+[2]*5+[3,4]*4+[5]*3+[6,7,8]*2+[9,10,11]*2+[12,13,14,15,16,17,18,19,20,22,24,25]
+  distance = random.choice(gains)
+  result = "gain"
+
+  if cpuMove == "Pass Defense":
+      # 10% chance of intercept if opponent is running pass Defense
+      if random.randint(1, 100) < 10:
+        result = "interception"
+        distance = 0
+      else:
+        # pass defense just makes pass less effective
+        distance = distance - random.randint(1, 5)
+        if distance < 0:
+          result = "loss"
+
+  return result, distance
+
+def fieldGoalAttempt(cpuMove, yardage):
+  """
+  Kick is either punt or field goal:
+  - punt: add as much distance as possible, turnover
+  - FG:   rolls the dice, odds are best between 15-35 yds 
+  """
+  
+  choice = input("Kick: Choose [p]unt or [f]ield Goal: ")
+  if choice == "p" or choice == "punt":
+    # go for a punt
+    distance = random.randrange(30, 50)
+    if cpuMove == "Kick Defense":
+      distance = distance - 10
+    result = "Turnover"
+  else: # go for FG
+    odds = 0 
+    if cpuMove == "Kick Defense":
+      odds = odds -0.20 # take off 20%
+    if yardage >= 15 and yardage <= 35:
+      odds = odds + 0.70 # add 70%
+    else:
+      odds = odds + 0.30
+    roll = random.random() # 0 - 1.0
+    if roll < odds:
+      result = "Field Goal"
+      distance = 0
+    else:
+      result = "Turnover"
+      distance = 0
+  # we have result and distance
+  return result, distance
+
+
 
 
 def main():
